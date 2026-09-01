311356cd07af5918336ad05a475888fd33add1ce
Kilian Koeltzsch <me@kilian.io>
2026-09-01 13:52:48 +0200

    initial commit: set up flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "TUI client for Actual Budget";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_26
+              pnpm
+            ];
+          };
+        }
+      );
+    };
+}
